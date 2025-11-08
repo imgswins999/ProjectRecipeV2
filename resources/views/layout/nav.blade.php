2748d069d6e2d9e527cf15d208798633cbcefa1b
Get226 <getzalow@gmail.com>
2025-11-08 21:29:59 +0700

    Hello world test

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'RecipeShare')</title>
     <link rel="stylesheet" href="{{ asset('../includes/css/navbar.css') }}">
+    <nav>Hello world</nav>
 </head>
 
 <body>
